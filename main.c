b50bd384fe0419901e5bed63047f0d81071b08cb
MARLEY018 <marleyantunes018@gmail.com>
2025-05-19 21:10:33 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include "carta.h"
 
+int main() {
+    Carta jogador = {"Dragão", 90, 70, 60};
+    Carta computador = {"Fênix", 85, 80, 75};
+
+    printf("Sua carta:\n");
+    mostrarCarta(jogador);
+
+    int escolha;
+    printf("\nEscolha um atributo para jogar (1-Força, 2-Velocidade, 3-Inteligência): ");
+    scanf("%d", &escolha);
+
+    printf("\nCarta do computador:\n");
+    mostrarCarta(computador);
+
+    int resultado = compararCartas(jogador, computador, escolha);
+
+    printf("\nResultado da rodada: ");
+    if (resultado == 1)
+        printf("Você venceu!\n");
+    else if (resultado == -1)
+        printf("Você perdeu!\n");
+    else
+        printf("Empate!\n");
+
+    return 0;
+}
