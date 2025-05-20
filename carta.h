42009c789082c194adf84358ed930408b9c7c31c
MARLEY018 <marleyantunes018@gmail.com>
2025-05-19 21:13:43 -0300

    Add files via upload

--- carta.h
+++ carta.h
@@ -1,1 +1,14 @@
+#ifndef CARTA_H
+#define CARTA_H
 
+typedef struct {
+    char nome[50];
+    int forca;
+    int velocidade;
+    int inteligencia;
+} Carta;
+
+void mostrarCarta(Carta carta);
+int compararCartas(Carta c1, Carta c2, int atributo);
+
+#endif
